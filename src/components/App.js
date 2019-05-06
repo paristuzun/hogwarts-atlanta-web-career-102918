b0725003a641521abb7efb6a4b05ef048b62926f
paristuzun <paristuzun@gmail.com>
2019-05-05 20:59:17 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import HogList from './HogList'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      filter: null
+    }
+  }
+
   render() {
     return (
       <div className="App">
